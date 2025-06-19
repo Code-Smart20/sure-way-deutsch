e598547de90e6df2d3e4084bdbd9ed936f0afcc3
Nwachukwu prince <bldrnwagod@gmail.com>
2025-06-19 11:45:04 -0700

    changed firebase

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,7 +22,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+
 
 
 
